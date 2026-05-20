c5974928ac39145d8fd93ff50cf9809d0746d2fc
leticiarau-byte <leticia_rau@estudante.sesisenai.org.br>
2026-05-20 19:40:16 -0300

    Mais funções na calculadora: 6-Raiz quadrada 7-quantos % B é de A 8-quantos % A é de B

--- calculadora.py
+++ calculadora.py
@@ -19,15 +19,23 @@ def divisao(a, b) :
         return "Divisão 0 "
     else: 
         return a/b 
+    
+def potencia(a,b) : 
+    return a ** b
+
+def raiz(a,b) : 
+    return a ** 0.5, b **0.5
+
+def porcentagem(a,b) : 
+    return a *(b/100)
    
 
 opcao = 1
 while opcao!=0: 
     
-    
     print("\n---calculando--- ")
 
-    opcao = int(input("Digite o que desejar: \n1-soma \n2-multiplicação \n3-divisão \n4-subtração\n0-Sair\n")) 
+    opcao = int(input("Digite o que desejar: \n1-soma \n2-multiplicação \n3-divisão \n4-subtração\n5-Potência\n6-Raiz quadrada\n7-quantos % B é de A\n0-Sair\n")) 
     
     if opcao != 0 :
         num1 = float(input("\nDigite o primeiro número: "))
@@ -47,7 +55,21 @@ while opcao!=0:
         
         elif opcao == 4: 
             resultado = subtracao(num1, num2)
-            print(f"\nSUBTRAÇÃO: Resultado: {resultado}\n")   
+            print(f"\nSUBTRAÇÃO: Resultado: {resultado}\n") 
+            
+        elif opcao == 5:
+            resultado = potencia(num1, num2)
+            print(f"\nPOTENCIA: Resultado: {resultado}\n") 
+            
+        elif opcao == 6:
+            resultado = raiz(num1, num2)
+            print(f"\nRAIZ QUADRADA: Resultado: {resultado}\n")
+        
+        elif opcao == 7:
+            resultado = porcentagem(num1, num2)
+            print(f"\nPORCENTAGEM DE B: Resultado: {resultado}\n")
+    
+             
     else : 
         print("SISTEMA FINALIZADO")
         
